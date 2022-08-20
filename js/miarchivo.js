084b4bdef528c985a86ae8e5210b307feb845b9b
gastongenaisir <gastongenaisir@gmail.com>
2022-08-20 15:33:58 -0300

    Desafío 1 CoderHouse Javascript

---
+++ js/miarchivo.js
@@ -0,0 +1,13 @@
+let numero = parseInt(prompt("Ingrese un número mayor a 1000"));
+if(numero > 1000){
+    alert(`El número ingresado es ${numero}`);
+}else if(numero > 1000){
+    alert("El número es menor a 1000");
+}
+
+let texto = (prompt("Ingrese Hola"));
+if (texto = "Hola"){
+    alert("Hola!")
+}else if ( texto (!Hola)){
+    alert("Por favor, ingresa Hola");
+}
